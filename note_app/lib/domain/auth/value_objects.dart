d2c5171ffd8c16efd165270fe6f0647803f8f175
RoozbehBandpey <r.bandpey67@gmail.com>
2021-09-21 23:02:12 +0200

    cleanups

--- note_app/lib/domain/auth/value_objects.dart
+++ note_app/lib/domain/auth/value_objects.dart
@@ -1,6 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:note_app/core/failures.dart';
 import 'package:note_app/core/value_objects.dart';
 import 'package:note_app/core/value_validators.dart';
@@ -8,10 +6,10 @@ import 'package:note_app/core/value_validators.dart';
 
 
 class EmailAddress extends ValueObject<String> {
+  @override
   final Either<ValueFailure<String>, String> value;
 
   factory EmailAddress(String input) {
-    assert(input != null);
     return EmailAddress._(
       validateEmailAddress(input),
     );
@@ -21,10 +19,10 @@ class EmailAddress extends ValueObject<String> {
 }
 
 class Password extends ValueObject<String> {
+  @override
   final Either<ValueFailure<String>, String> value;
 
   factory Password(String input) {
-    assert(input != null);
     return Password._(
       validatePassword(input),
     );
